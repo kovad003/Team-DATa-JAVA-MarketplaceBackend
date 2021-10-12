2a06259e0f717e6f28edfcb03bfb337d05af1654
Daniel Kovacs <kovacsdansan@gmail.com>
2021-10-13 01:14:13 +0300

    login system is working, still need some improvement
    Future improvements:
     - hashing passwords
     - simplify the process of login data handling

--- webshop/src/main/java/services/LoginService.java
+++ webshop/src/main/java/services/LoginService.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,6 +25,66 @@ import data.Login;
  */
 @Path("/loginservice")
 public class LoginService {
+//	*********************************************************************************************************
+//	POST SERVICES ********************************************************************************************
+//	*********************************************************************************************************
+	/**
+	 * @return 
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/validatelogindetails")
+	public Login validateLoginDetails(Login input) {
+		// Debugging messages
+		System.out.println("public Login checkLoginDetails(Login login) {");
+		System.out.println("Login input object: " + input.getUserName() + " & " + input.getPassword());
+
+        // Get Connection to DB
+        Connection conn = null;
+		try {
+		    if (SystemProperty.environment.value() ==SystemProperty.Environment.Value.Production) {  
+		    	conn = Connections.getProductionConnection();
+		    }
+		    else {
+		    	conn = Connections.getDevConnection();
+		    }
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        // Initialise Empty Objects for Data Transfer
+        String sql = "SELECT customerId, userName FROM customer WHERE userName = ? AND `password` = ?;"; // Define a MySQL query
+
+        PreparedStatement pstmt; // Will deliver query to DB
+		ResultSet RS = null; // Will deliver data back to JAVA from DB
+		Login login = new Login(); // Constructor for Login object => Will be amended with the data from DB			
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, input.getUserName());
+			pstmt.setString(2, input.getPassword());
+			RS = pstmt.executeQuery();			
+			while (RS.next()) {
+				login.setCustomerId(RS.getInt("customerId"));
+				login.setUserName(RS.getString("userName")); // ASH: it is necessary to amend script file with 'password' at `password column`
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (conn!=null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("customerId: " + login.getCustomerId());
+		System.out.println("userName: " + login.getUserName());
+		return login;
+	}
 //	*********************************************************************************************************
 //	GET SERVICES ********************************************************************************************
 //	*********************************************************************************************************
