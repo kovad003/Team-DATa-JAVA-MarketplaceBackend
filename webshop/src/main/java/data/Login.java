af6fb9b8444ff231e7f5bfaf9b6f177296fed998
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2021-10-23 13:56:51 +0300

    AD - added and amended comments to various backend files
    AD - added and amended comments to various backend files

--- webshop/src/main/java/data/Login.java
+++ webshop/src/main/java/data/Login.java
@@ -4,7 +4,7 @@ package data;
 import java.sql.Timestamp;
 
 /**
- * @author Ash
+ * @author Dan
  *	Data class for "customer" table with the purpose of user authentication
  */
 public class Login {
